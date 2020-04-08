b08b34041b6562fc814a242ddf79a5124b495f6b
KiNG <60117703+theKING05@users.noreply.github.com>
2020-04-08 13:47:24 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+/*Notes
+<script>
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+</script>
+
+//....in javascript, you...
+/*cannot b a reserved keyword eg: typeof
+should be meaningful
+cannot start with a number
+are case-sensitive
+
+/*...Primitive/value types
+let name ="King"; ....//string literal
+let age ="20"; .....//number literal
+let isApproved = false; ....//boolean
+let firstName = undefined;
+let selectedColor = null;
+*/
